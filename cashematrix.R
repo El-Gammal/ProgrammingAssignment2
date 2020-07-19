8639dbfc68edeeb8f6b5dfa7f548a7323ee35d9f
El-Gammal <66030537+El-Gammal@users.noreply.github.com>
2020-07-19 14:14:55 +0200

    Add files via upload

--- cashematrix.R
+++ cashematrix.R
@@ -1,11 +1,29 @@
-set.seed(1)
-n <- 1000
-sAve <- vector("numeric",n)
-for (i in 1:n) {
-  s5 <- sample(x,5)
-  sAve[i] <- mean(s5)
+makeCacheMatrix <- function(x = matrix()) {
+     i <- NULL
+     set <- function(y) {
+         x <<- y
+         i <<- NULL
+     }
+     get <- function() x
+     setinverse <- function(inverse) i <<- inverse
+     getinverse <- function() i
+     list(set = set,
+          get = get,
+          setinverse = setinverse,
+          getinverse = getinverse)
 }
-hist(sAve)
-mean(abs(sAve-mean(x))>1)
-mad_libs()
-rea
+
+cacheSolve <- function(x, ...) {
+       i <- x$getinverse()
+       if (!is.null(i)) {
+             message("getting cached data")
+             return(i)
+     }
+       data <- x$get()
+       i <- solve(data, ...)
+       x$setinverse(i)
+       i
+}
+B <- matrix(c(1,2,3,4),2,2)
+B1 <- makeCacheMatrix(B)
+cacheSolve(B1)
